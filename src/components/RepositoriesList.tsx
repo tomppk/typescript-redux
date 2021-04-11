eb668f5355cecbe7b77dc8e975b69586e30dabc9
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-11 10:10:10 +0300

    add functionality to dispatch search term to action creator inside repositorieslist component

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,15 +1,25 @@
+// useDispatch hook gives us access to dispatch() actions directly inside our
+// component. We can useDispatch to manually dispatch an action creator
+// We need to import our action creators as well
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { actionCreators } from '../redux';
 
 // Annotate type as React.FunctionalComponent
 const RepositoriesList: React.FC = () => {
   // Define local piece of state with default value of empty string
+  // Define dispatch hook to use it
   const [term, setTerm] = useState('');
+  const dispatch = useDispatch();
 
   // Get correct type for event by ctrl+click event inside form ie.
   // <form onSubmit={e => console.log(e)}
-  // Prevent default form submission
+  // Prevent default form submission.
+  // Dispatch our search term to our searchRepositories action creator
   const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
+
+    dispatch(actionCreators.searchRepositories(term));
   };
 
   // Controlled input with local state term. Term is set to be the value
