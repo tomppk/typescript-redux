691ee7622e25d9bd95abd2c3c1cdeb8265ab38de
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-11 10:28:56 +0300

    connect useActions hook to repositorieslist component

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,16 +1,18 @@
-// useDispatch hook gives us access to dispatch() actions directly inside our
-// component. We can useDispatch to manually dispatch an action creator
-// We need to import our action creators as well
+// Our useActions custom hook gives us access to dispatch() actions directly
+// inside our component. We can use it to manually dispatch an action creator
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { actionCreators } from '../redux';
+import { useActions } from '../hooks/useActions';
 
 // Annotate type as React.FunctionalComponent
 const RepositoriesList: React.FC = () => {
   // Define local piece of state with default value of empty string
-  // Define dispatch hook to use it
+  // Destructure searchRepositories action creator out of useActions hook
+  // searchRepositories is 'bound' with bindActionCreator so its return value
+  // is automatically dispatched
+  // With this hook we do not need to use following inside this component
+  // dispatch(actionCreators.searchRepositories(term));
   const [term, setTerm] = useState('');
-  const dispatch = useDispatch();
+  const { searchRepositories } = useActions();
 
   // Get correct type for event by ctrl+click event inside form ie.
   // <form onSubmit={e => console.log(e)}
@@ -19,7 +21,7 @@ const RepositoriesList: React.FC = () => {
   const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
 
-    dispatch(actionCreators.searchRepositories(term));
+    searchRepositories(term);
   };
 
   // Controlled input with local state term. Term is set to be the value
