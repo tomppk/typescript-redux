c34d652bd9d0f4750342f6121ffc44c2b3f438ae
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-11 09:53:28 +0300

    create initial RepositoriesList component

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,13 @@
+// Annotate type as React.FunctionalComponent
+const RepositoriesList: React.FC = () => {
+  return (
+    <div>
+      <form>
+        <input />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default RepositoriesList;
