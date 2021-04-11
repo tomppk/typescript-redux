85dea611dacca89c2908d72b3eb78f87c2ab0453
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-11 09:52:21 +0300

    import App component into index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-export const App = () => {
-  return <div>Hi there!</div>;
-};
+import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
